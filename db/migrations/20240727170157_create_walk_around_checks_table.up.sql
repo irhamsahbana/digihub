412904b342e55d61e1b6845eca5b93cf19e54045
Irham Sahbana <irhamsahbana@gmail.com>
2024-08-05 20:50:56 +0800

    chore: Add columns for branch, section, and user IDs in walk_around_checks table

--- db/migrations/20240727170157_create_walk_around_checks_table.up.sql
+++ db/migrations/20240727170157_create_walk_around_checks_table.up.sql
@@ -1,5 +1,8 @@
 CREATE TABLE IF NOT EXISTS walk_around_checks (
     id CHAR(26) PRIMARY KEY,
+    branch_id CHAR(26) NOT NULL,
+    section_id CHAR(26) NOT NULL,
+    user_id CHAR(26) NOT NULL,
     client_id CHAR(26) NOT NULL,
     is_offered BOOLEAN DEFAULT FALSE NOT NULL,
     is_need_follow_up BOOLEAN DEFAULT FALSE NOT NULL,
@@ -7,5 +10,8 @@ CREATE TABLE IF NOT EXISTS walk_around_checks (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP WITH TIME ZONE,
 
+    FOREIGN KEY (branch_id) REFERENCES branches (id),
+    FOREIGN KEY (section_id) REFERENCES sections (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (client_id) REFERENCES clients (id)
 );
